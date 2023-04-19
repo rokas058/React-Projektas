12666d530b977abe4b8e64ad5d6be5d2654aeb93
unknown <migleker@gmail.com>
2023-04-19 15:48:10 +0300

    pirkimas i krepseli

--- src/components/ShoppingCartButton.js
+++ src/components/ShoppingCartButton.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const ShoppingCartButton = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
   return (
-    <Button variant="light" className="shopping-cart-btn">
-      <FontAwesomeIcon icon={faShoppingCart} />
-    </Button>
+    <div>
+      <Link to="/cart" className="shopping-cart-btn">
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </Link>
+      {cartItems.map((item, index) => (
+        <div key={index}>{item.pavadinimas}</div>
+      ))}
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ShoppingCartContext } from '../src/components/ShoppingCartContext';
+
+const initialState = {
+  cartItems: [],
+};
+
+const shoppingCartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      return { ...state, cartItems: [...state.cartItems, action.payload] };
+    case 'REMOVE_ITEM':
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(item => item.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+const AppWrapper = () => {
+  const [state, dispatch] = useReducer(shoppingCartReducer, initialState);
+
+  return (
+    <ShoppingCartContext.Provider value={{ ...state, addToCart: item => dispatch({ type: 'ADD_ITEM', payload: item }), removeFromCart: id => dispatch({ type: 'REMOVE_ITEM', payload: id }) }}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ShoppingCartContext.Provider>
+  );
+};
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+root.render(<AppWrapper />);
